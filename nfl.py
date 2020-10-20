b8c4a293c07993792fcebe2193ae06ab9e4864f9
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-10-19 22:08:24 -0700

    Add nfl configuration class

---
+++ nfl.py
@@ -0,0 +1,17 @@
+class NFL:
+    league_id = 16
+    sport_id = 4
+    default_market_id = 401  # pointspread
+    current_season_id = 8582
+    # self.market_ids = {
+    #     "ml": ,
+    #     "ml_1h": ,
+    #     "ml_2h": ,
+    #     "ml_1q": ,
+    #     "ml_2q": ,
+    #     "ml_": ,
+    #     "": ,
+    #     "": ,
+    #     "": ,
+    #     "": ,
+    # }
