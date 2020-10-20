93a1ff27385e4f5647b2800859f8c9d7c75732bd
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-10-20 01:17:59 -0700

    Add nfl teams

--- nfl.py
+++ nfl.py
@@ -1,17 +1,11 @@
+import json
+
+
 class NFL:
     league_id = 16
     sport_id = 4
     default_market_id = 401  # pointspread
     current_season_id = 8582
-    # self.market_ids = {
-    #     "ml": ,
-    #     "ml_1h": ,
-    #     "ml_2h": ,
-    #     "ml_1q": ,
-    #     "ml_2q": ,
-    #     "ml_": ,
-    #     "": ,
-    #     "": ,
-    #     "": ,
-    #     "": ,
-    # }
+
+    with open("json/nfl_teams.json") as f:
+        teams = json.load(f)
