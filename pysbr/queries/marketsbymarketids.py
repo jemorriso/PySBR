a435dd5bfb981849e2fc7ee5bb666c77382c7cb9
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-11-09 12:36:42 -0800

    Add MarketsByMarketIds query

---
+++ pysbr/queries/marketsbymarketids.py
@@ -0,0 +1,13 @@
+from pysbr.queries.query import Query
+
+
+class MarketsByMarketIds(Query):
+    def __init__(self, market_ids, sport_id):
+        super().__init__()
+        self.name = "marketTypesById"
+        self.arg_str = self._get_args("market_ids")
+        self.args = {"mtids": market_ids, "spids": [sport_id]}
+        self.fields = self._get_fields("markets_by_id")
+        self._raw = self._build_and_execute_query(
+            self.name, self.fields, self.arg_str, self.args
+        )
