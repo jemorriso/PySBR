44166db7e5bdcd51577342928bbd3ff30226ea28
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-11-08 23:57:10 -0800

    Add EventGroupsByLeague query

---
+++ pysbr/queries/eventgroupsbyleague.py
@@ -0,0 +1,13 @@
+from pysbr.queries.query import Query
+
+
+class EventGroupsByLeague(Query):
+    def __init__(self, league_id):
+        super().__init__()
+        self.name = "eventGroupsByLeague"
+        self.arg_str = self._get_args("league_id")
+        self.args = {"lid": league_id}
+        self.fields = self._get_fields("event_group")
+        self._raw = self._build_and_execute_query(
+            self.name, self.fields, self.arg_str, self.args
+        )
