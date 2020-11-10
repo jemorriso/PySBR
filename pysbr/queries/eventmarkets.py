3d059fe047edff8d565445c6b622ed29653c6b9d
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-11-09 16:53:17 -0800

    Add EventMarkets query

---
+++ pysbr/queries/eventmarkets.py
@@ -0,0 +1,13 @@
+from pysbr.queries.query import Query
+
+
+class EventMarkets(Query):
+    def __init__(self, event_id):
+        super().__init__()
+        self.name = "eventMarkets"
+        self.arg_str = self._get_args("event_id")
+        self.args = {"eid": event_id}
+        self.fields = self._get_fields("market_types")
+        self._raw = self._build_and_execute_query(
+            self.name, self.fields, self.arg_str, self.args
+        )
