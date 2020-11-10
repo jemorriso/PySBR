a9d46e7b0473830a59fa06e43695334af61e6ad9
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-11-09 16:15:40 -0800

    Add SearchSports query

---
+++ pysbr/queries/searchsports.py
@@ -0,0 +1,14 @@
+from pysbr.queries.query import Query
+
+
+# only returns five first results, and only for upcoming events
+class SearchSports(Query):
+    def __init__(self, search_term):
+        super().__init__()
+        self.name = "multipleSearch"
+        self.arg_str = self._get_args("search_sport")
+        self.args = {"search_term": [search_term]}
+        self.fields = self._get_fields("search_sport")
+        self._raw = self._build_and_execute_query(
+            self.name, self.fields, self.arg_str, self.args
+        )
