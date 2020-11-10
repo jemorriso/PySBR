2eb8311c7405d1b3292a2509a021e4d9db102dfe
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-11-09 21:44:26 -0800

    Add EventsByMatchup query

---
+++ pysbr/queries/eventsbymatchup.py
@@ -0,0 +1,17 @@
+from pysbr.queries.query import Query
+
+
+class EventsByMatchup(Query):
+    def __init__(self, participant_id1, participant_id2, count):
+        super().__init__()
+        self.name = "lastMatchupsByParticipants"
+        self.arg_str = self._get_args("matchup")
+        self.args = {
+            "partid1": participant_id1,
+            "partid2": participant_id2,
+            "limit": count,
+        }
+        self.fields = self._get_fields("event")
+        self._raw = self._build_and_execute_query(
+            self.name, self.fields, self.arg_str, self.args
+        )
