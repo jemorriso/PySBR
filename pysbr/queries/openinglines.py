eb251bd5c15084bee0c7632f13b222448a749427
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-11-09 22:28:02 -0800

    Add OpeningLines query

---
+++ pysbr/queries/openinglines.py
@@ -0,0 +1,13 @@
+from pysbr.queries.query import Query
+
+
+class OpeningLines(Query):
+    def __init__(self, event_ids, market_ids):
+        super().__init__()
+        self.name = "openingLines"
+        self.arg_str = self._get_args("lines")
+        self.args = {"eids": event_ids, "mtids": market_ids}
+        self.fields = None
+        self._raw = self._build_and_execute_query(
+            self.name, q_arg_str=self.arg_str, q_args=self.args
+        )
