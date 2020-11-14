f265ce20d0915de651ae1f236144332af53e9cc1
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-11-13 19:19:08 -0800

    Add EventsByParticipants query
    Subquery necessary in order to filter by participant, since there is no
    suitable graphql query from the endpoint.
    
    This functionality is too important to pass up.

--- pysbr/queries/eventsbyparticipants.py
+++ pysbr/queries/eventsbyparticipants.py
@@ -1,13 +1,23 @@
 from pysbr.queries.query import Query
+from pysbr.utils import Utils
 
 
 class EventsByParticipants(Query):
-    def __init__(self, participant_ids):
-        # this query only gets the 5 most recent events for a participant.
+    def __init__(self, participant_ids, start, end, league_id=None, sport_id=None):
+        if league_id is None and sport_id is None:
+            raise ValueError("Either league_id or sport_id must not be None.")
+
         super().__init__()
-        self.name = "eventsInfoByParticipant"
-        self.arg_str = self._get_args("participants")
-        self.args = {"partids": participant_ids}
+        event_ids = self._filter_events(
+            participant_ids, start, end, league_id, sport_id
+        )
+        if not event_ids:
+            return None
+
+        self.name = "eventsV2"
+        self.arg_str = self._get_args("event_ids")
+        # TODO
+        self.args = {"eids": event_ids}
         self.fields = self._get_fields("event")
         self._raw = self._build_and_execute_query(
             self.name, self.fields, self.arg_str, self.args
@@ -17,10 +27,43 @@ class EventsByParticipants(Query):
         self._sublist_keys = ["participants"]
         self._id_key = "event id"
 
-    def _find_data(self):
-        data = self._raw[self.name]
-        cleaned_data = []
-        for participant in data:
-            cleaned_data.extend(participant["events"])
+    def _league_args(self, start, end, league_id):
+        q_arg_str = self._get_args("date_range")
+        q_args = {
+            "lids": [league_id],
+            "start": Utils.datetime_to_timestamp(start),
+            "end": Utils.datetime_to_timestamp(end),
+        }
+        return q_arg_str, q_args
 
-        return cleaned_data
+    def _sport_args(self, start, end, sport_id):
+        q_arg_str = self._get_args("date_range_sport")
+        q_args = {
+            "spids": [sport_id],
+            "start": Utils.datetime_to_timestamp(start),
+            "end": Utils.datetime_to_timestamp(end),
+        }
+        return q_arg_str, q_args
+
+    def _filter_events(self, participant_ids, start, end, league_id, sport_id):
+        q_name = "eventsV2"
+        q_arg_str, q_args = (
+            self._league_args(start, end, league_id)
+            if league_id is not None
+            else self._sport_args(start, end, sport_id)
+        )
+        q_fields = self._get_fields("event_2")
+        raw = self._build_and_execute_query(q_name, q_fields, q_arg_str, q_args)
+        # filter out relevant events
+
+        ids = []
+        for e in raw["eventsV2"]["events"]:
+            try:
+                event_pids = [p["partid"] for p in e["participants"]]
+                for pid in participant_ids:
+                    if pid in event_pids:
+                        ids.append(e["eid"])
+            except KeyError:
+                pass
+
+        return ids
