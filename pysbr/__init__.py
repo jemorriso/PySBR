542dc732f6519b3ba78d8eccdd624e76f1af89d2
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-12-01 10:10:37 -0800

    Add license, update setup.py and pysbr/__init__.py

---
+++ pysbr/__init__.py
@@ -0,0 +1,48 @@
+from pysbr.queries.bestlines import BestLines
+from pysbr.queries.consensusHistory import ConsensusHistory
+from pysbr.queries.currentlines import CurrentLines
+from pysbr.queries.eventgroupsbyleague import EventGroupsByLeague
+from pysbr.queries.eventmarkets import EventMarkets
+from pysbr.queries.eventsbydate import EventsByDate
+from pysbr.queries.eventsbydaterange import EventsByDateRange
+from pysbr.queries.eventsbyeventids import EventsByEventIds
+from pysbr.queries.eventsbymatchup import EventsByMatchup
+from pysbr.queries.eventsbyparticipants import EventsByParticipants
+from pysbr.queries.eventsbyparticipantsrecent import EventsByParticipantsRecent
+from pysbr.queries.leaguehierarchy import LeagueHierarchy
+from pysbr.queries.leaguemarkets import LeagueMarkets
+from pysbr.queries.leaguesbyleagueids import LeaguesByLeagueIds
+from pysbr.queries.linehistory import LineHistory
+from pysbr.queries.marketsbymarketids import MarketsByMarketIds
+from pysbr.queries.openinglines import OpeningLines
+from pysbr.queries.searchevents import SearchEvents
+from pysbr.queries.searchleagues import SearchLeagues
+from pysbr.queries.searchsports import SearchSports
+from pysbr.queries.sportsbooks import Sportsbooks
+from pysbr.queries.team import Team
+
+from pysbr.config.sport import (
+    Football,
+    Basketball,
+    Baseball,
+    Hockey,
+    Soccer,
+    Tennis,
+    Fighting,
+)
+from pysbr.config.sport import (
+    NFL,
+    NCAAF,
+    MLB,
+    NBA,
+    NCAAB,
+    NHL,
+    EPL,
+    UCL,
+    LaLiga,
+    Bundesliga,
+    UEFANationsLeague,
+    ATP,
+    UFC,
+)
+from pysbr.config.sportsbook import Sportsbook
